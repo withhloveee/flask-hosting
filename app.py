3a1c795852d716e8f1a120acd3d3289e2881698c
Aditya <167296292+withhloveee@users.noreply.github.com>
2025-11-14 11:32:36 +0530

    Refactor Flask app to use environment port

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
-    return "Hello, Railway!"
+    return "Hello!"
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
